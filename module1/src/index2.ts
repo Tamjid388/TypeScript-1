5e1e29e1c8e17b4d59b6bf9bcfa6261c5432252a
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-01 00:48:51 +0600

    Objects

---
+++ module1/src/index2.ts
@@ -0,0 +1,12 @@
+// Reference Type -- Object
+const user:{
+    nationality:"Bangladeshi",  //Type-Literal Types
+    firstname:string,
+    lastname?:string, //Optional "?" Type
+    nickname:string
+}={ 
+    nationality:"Bangladeshi",
+    firstname:"Tamjid ",
+    nickname:"Razin"
+}
+console.log(user.firstname);
