4f9efce81536de4f96a7ceef28e80f609fdb0cb8
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-01 00:17:43 +0600

    first commit

---
+++ module1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let studentname = "tamjid ahmed";
+console.log(studentname);
---
+++ module1/src/index.ts
@@ -0,0 +1,21 @@
+let studentname:string="tamjid ahmed";
+console.log(studentname);
+
+// basic Data-types
+let firstname:string="tamjid Ahmed"
+let rollNum : number=123;
+let isAdmin :boolean=false
+
+let x:undefined=undefined;
+let y:null=null;
+
+let d;
+
+// Array In TypeScript
+let friends:string[]=["tamjid","John"]
+
+// Tuple In TypeScript
+let test:[number,number]=[1,5]
+
+let test2:[string,number]=["nam",2]
+let test3=["nam",2]
