302d87ba73dd2734501f99c1c82b3d1511ee91b6
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-08 01:28:25 +0600

    Generic & Interface

--- module1/src/Generic&Interface.ts
+++ module1/src/Generic&Interface.ts
@@ -1,6 +1,58 @@
 {
     // Generic & Interface
 
+    interface Developer<T> {
+        name: string,
+        device: {
+            brand: string,
+            model: string,
+            Year: number
+        },
+        smartWatch: T
+    }
+
+    const developer: Developer<{
+        name: string,
+        model: string,
+        display: string
+    }> =
+    {
+
+        name: "Alpha",
+        device: {
+            brand: "AsRock",
+            model: "Ex",
+            Year: 2022
+        },
+        smartWatch: {
+            name: "AmazFit",
+            model: "A2",
+            display: "Oled"
+
+        }
+
+    }
+    const developer2: Developer<{
+        name: string,
+        model: string,
+        display: string,
+        batteryLife: string,
+        waterResistant: boolean
+    }> = {
+        name: "Beta",
+        device: {
+            brand: "MSI",
+            model: "ProMax",
+            Year: 2024
+        },
+        smartWatch: {
+            name: "Galaxy Watch",
+            model: "5 Pro",
+            display: "AMOLED",
+            batteryLife: "3 days",
+            waterResistant: true,
+        }
+    };
 
 
 
@@ -11,7 +63,4 @@
 
 
 
-
-
-    
 }
